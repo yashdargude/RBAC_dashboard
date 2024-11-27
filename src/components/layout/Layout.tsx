e063e9afe451aee09aef345cc04a2081d6c2650e
YashProacure <yash@proacure.com>
2024-11-27 23:46:40 +0530

    Update role form and permission matrix

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen bg-background">
-      <Header className="border-b" />
+      <Header />
       <div className="flex h-[calc(100vh-4rem)]">
         <Sidebar />
         <main className="flex-1 overflow-y-auto p-8">
